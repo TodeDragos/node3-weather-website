2c73da810adfaafb365e9977559c81359d96bfc9
TodeDragos <dragos.tode@gmail.com>
2019-07-18 20:22:15 +0300

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 function forecast (longitude, latitude, callback)
 {
-    const url = `https://api.darksky.net/forecast/ec5f8bf831d5d44ead41c739482efe32/${longitude},${latitude}?units=si&lang=ro`;
+    const url = `https://api.darksky.net/forecast/ec5f8bf831d5d44ead41c739482efe32/${longitude},${latitude}?units=si`;
 
     request({ url, json:true }, (error, { body }) => {
         if (error)
@@ -10,7 +10,7 @@ function forecast (longitude, latitude, callback)
         else if (body.error)
             callback('Unable to find location!', undefined);
         else
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} change of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureMax} with a low of ${body.daily.data[0].temperatureMin}. There is a ${body.currently.precipProbability} change of rain`)
     })
 }
 
